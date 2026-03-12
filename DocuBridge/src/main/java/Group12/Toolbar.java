6b3f418e4efb799810e816827823fd2b499e08ff
Abdulrehman Nasir <abdul.n4444@gmail.com>
2026-03-12 17:27:13 -0400

    added buttons
    Added front-end Buttons to program, currently basic, will change look later.

--- DocuBridge/src/main/java/Group12/Toolbar.java
+++ DocuBridge/src/main/java/Group12/Toolbar.java
@@ -1,21 +1,68 @@
 package Group12;
 
 import javafx.stage.Screen;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 public class Toolbar {
+
     private double dpi;
+    private VBox toolbarContainer;
 
     private double readDPI(){
         Screen s = Screen.getPrimary();
-        return (s.getDpi());
+        return s.getDpi();
     }
+
     public Toolbar() {
-        dpi =  readDPI();
-        //fill code here
+        dpi = readDPI();
+        createToolbar();
+    }
+
+    private void createToolbar(){
+
+        MenuBar menuBar = new MenuBar();
+
+        // File Menu
+        Menu file = new Menu("File");
+        file.getItems().addAll(
+                new MenuItem("New"),
+                new MenuItem("Open"),
+                new MenuItem("Save"),
+                new MenuItem("Save As")
+        );
+
+        // Edit Menu
+        Menu edit = new Menu("Edit");
+        edit.getItems().addAll(
+                new MenuItem("Undo"),
+                new MenuItem("Redo"),
+                new MenuItem("Find")
+        );
+
+        // Insert Menu
+        Menu insert = new Menu("Insert");
+        insert.getItems().addAll(
+                new MenuItem("Image"),
+                new MenuItem("Table")
+        );
+
+        // Format menu
+        Menu format = new Menu("Format");
+
+        MenuItem alignLeft = new MenuItem("Align Left");
+        MenuItem alignCenter = new MenuItem("Align Center");
+        MenuItem alignRight = new MenuItem("Align Right");
+
+        format.getItems().addAll(alignLeft, alignCenter, alignRight);
+
+        // add menus to toolbar
+        menuBar.getMenus().addAll(file, edit, insert, format);
+
+        toolbarContainer = new VBox(menuBar);
+    }
+
+    public VBox getView(){
+        return toolbarContainer;
     }
 }
-
-/*  Look into JavaFX MenuBar & ButtonBar
-    MenuBar for the things like File, Edit, Insert, Format, etc
-    ButtonBar for the buttons like bold, italics, etc
- */
