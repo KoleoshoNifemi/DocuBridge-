8738b22b44d05038fe06d1fb3ac100411673191f
Shehryar Usman <94995564+ShehryarUsman007@users.noreply.github.com>
2026-03-12 10:50:03 -0400

    Added dots per inch (allows for exact sizing)

---
+++ DocuBridge/src/main/java/Group12/Editor.java
@@ -0,0 +1,80 @@
+package Group12;
+import javafx.stage.Screen;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
+
+public class Editor {
+    private String name;
+    private WebView webView;
+    private WebEngine quill;
+    private ScrollPane scrollPane;
+    private StackPane webViewContainer;
+    private BorderPane mainLayout;
+    private ToolBar toolBar;
+    private double dpi;
+
+    private double readDPI(){
+        Screen s = Screen.getPrimary();
+        return (s.getDpi());
+    }
+
+    private void initializeWebView(){
+        webView = new WebView();
+        quill = webView.getEngine();
+        String quillJsPath = getClass().getResource("/quill/editor.html").toExternalForm();
+        quill.load(quillJsPath);
+
+        webView.setPrefHeight(800);
+        webView.setMaxWidth(8.5 * dpi);     //8.5 inches width
+        webView.setMinHeight(600);
+    }
+
+    private void StyleContainer(){
+        webViewContainer = new StackPane();
+        webViewContainer.setStyle("-fx-background-color: rgb(204, 202, 202)" + "-fx-padding: 30px 0;");
+        webViewContainer.setMaxWidth(Double.MAX_VALUE);
+        webViewContainer.setStyle( "-fx-effect: dropshadow(gaussian, rgba(0,0,0,0.1), 10, 0, 0, 2);");
+        webViewContainer.setAlignment(javafx.geometry.Pos.CENTER);
+        webViewContainer.getChildren().add(webView);
+
+
+        scrollPane = new ScrollPane();
+        scrollPane.setContent(webViewContainer);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    }
+
+    private void initializeLayout(){
+        mainLayout = new BorderPane();
+        // mainLayout.setTop() -> toolbar would be param
+        mainLayout.setCenter(scrollPane);
+    }
+
+    public Editor(String name) {
+        this.name = name;
+        dpi = readDPI();
+        //initialize the toolbar here
+        initializeWebView();
+        StyleContainer();
+        initializeLayout();
+
+    }
+
+    //Getters & Setters
+    public BorderPane getView() {
+        return mainLayout;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDPI(){
+        return dpi;
+    }
+}
--- DocuBridge/src/main/java/Group12/Toolbar.java
+++ DocuBridge/src/main/java/Group12/Toolbar.java
@@ -1,9 +1,21 @@
 package Group12;
 
+import javafx.stage.Screen;
 
 public class Toolbar {
+    private double dpi;
 
+    private double readDPI(){
+        Screen s = Screen.getPrimary();
+        return (s.getDpi());
+    }
     public Toolbar() {
+        dpi =  readDPI();
         //fill code here
     }
 }
+
+/*  Look into JavaFX MenuBar & ButtonBar
+    MenuBar for the things like File, Edit, Insert, Format, etc
+    ButtonBar for the buttons like bold, italics, etc
+ */
