312db79cc740b4f1292e2d3bbc9457ac6636bfd9
Shehryar Usman <94995564+ShehryarUsman007@users.noreply.github.com>
2026-03-12 18:14:12 -0400

    Further Refactoring

--- DocuBridge/src/main/java/Group12/Toolbar.java
+++ DocuBridge/src/main/java/Group12/Toolbar.java
@@ -9,11 +9,6 @@ public class Toolbar {
     private double dpi;
     private VBox toolbarContainer;
 
-    public Toolbar() {
-        dpi = readDPI();
-        createToolbar();
-    }
-
     private double readDPI() {
         return Screen.getPrimary().getDpi();
     }
@@ -21,24 +16,29 @@ public class Toolbar {
     private void createToolbar() {
         MenuBar menuBar = new MenuBar();
 
-        // add all the menus
+        // Add all the menus
         menuBar.getMenus().addAll(
-                createMenu("File", "New", "Open", "Save", "Save As"),
-                createMenu("Edit", "Undo", "Redo", "Find"),
-                createMenu("Insert", "Image", "Table"),
-                createMenu("Format", "Align Left", "Align Center", "Align Right")
+                createMenu("File", new String[] {"New", "Open", "Save", "Save As"}),
+                createMenu("Edit", new String[]{"Undo", "Redo", "Find"}),
+                createMenu("Insert", new String[]{"Image", "Table"}),
+                createMenu("Format", new String[]{"Align Left", "Align Center", "Align Right"})
         );
 
         toolbarContainer = new VBox(menuBar);
     }
 
-    // create Menu with items
-    private Menu createMenu(String menuName, String... itemNames) {
+    // Create Menu with items
+    private Menu createMenu(String menuName, String[] itemNames) {
         Menu menu = new Menu(menuName);
         for (String name : itemNames) {
             menu.getItems().add(new MenuItem(name));
         }
         return menu;
+    }
+
+    public Toolbar() {
+        dpi = readDPI();
+        createToolbar();
     }
 
     public VBox getView() {
