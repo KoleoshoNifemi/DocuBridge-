08d6cab8dd1173dc6b51037dcbd74ae5bbfd74b4
Shehryar Usman <94995564+ShehryarUsman007@users.noreply.github.com>
2026-03-11 20:41:28 -0400

    Fixed Git issues on my computer.
    Base project now ready to be downloaded

---
+++ DocuBridge/src/main/java/Group12/DocumentController.java
@@ -0,0 +1,16 @@
+package Group12;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DocumentController extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
