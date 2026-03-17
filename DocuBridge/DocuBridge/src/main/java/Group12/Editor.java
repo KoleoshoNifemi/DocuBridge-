06ced8cae5816c7a671706c9e53550ba617229ed
Shehryar Usman <94995564+ShehryarUsman007@users.noreply.github.com>
2026-03-16 22:06:51 -0400

    Basic Shortcuts and ToolBar buttons working
    More to be added...

---
+++ DocuBridge/DocuBridge/src/main/java/Group12/Editor.java
@@ -0,0 +1,278 @@
+package Group12;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Screen;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
+import javafx.util.Duration;
+
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+
+public class Editor {
+    private String name;
+    private WebView webView;
+    private WebEngine quill;
+    private ScrollPane scrollPane;
+    private StackPane webViewContainer;
+    private BorderPane mainLayout;
+    private Toolbar toolBar;
+    private double dpi;
+    private ClipboardHandler clipboardHandler;
+
+    private double readDPI(){
+        return Screen.getPrimary().getDpi();
+    }
+
+    // Getting our WebView running with quillJS
+    private void initializeWebView() {
+        webView = new WebView();
+        quill = webView.getEngine();
+        String quillJsPath = getClass().getResource("/quill/editor.html").toExternalForm();
+        quill.getLoadWorker().stateProperty().addListener((obs, old, newState) -> {
+            if (newState == javafx.concurrent.Worker.State.SUCCEEDED) {
+                // HTML loaded – now wait for Quill to be ready
+                waitForQuillReady();
+            }
+        });
+        quill.load(quillJsPath);
+
+        webView.setPrefHeight(800);
+        webView.setMaxWidth(8.5 * dpi);     //8.5 inches width
+        webView.setMinHeight(600);
+    }
+
+    private void waitForQuillReady() {
+        PauseTransition pause = new PauseTransition(Duration.millis(100));
+        pause.setOnFinished(event -> {
+            // Execute JavaScript to test if Quill is defined and not null
+            Boolean quillReady = (Boolean) quill.executeScript("typeof quill !== 'undefined' && quill !== null");
+            if (Boolean.TRUE.equals(quillReady)) {  //Quill is fully loaded internally
+                clipboardHandler = new ClipboardHandler(webView);   // Initialize clipboard handler (handles all copy/cut/paste functionality)
+                //clipboardHandler.enableCharacterByCharacterUndo();
+                initializeShortcuts();
+
+            } else {
+                // Not ready yet – try again
+                waitForQuillReady();
+            }
+        });
+        pause.play();
+    }
+
+    private void StyleContainer(){
+        // Configuring the stackPane which holds webView
+        webViewContainer = new StackPane();
+        webViewContainer.setStyle("-fx-background-color: rgb(204, 202, 202)" + "-fx-padding: 30px 0;");
+        webViewContainer.setMaxWidth(Double.MAX_VALUE);
+        webViewContainer.setStyle( "-fx-effect: dropshadow(gaussian, rgba(0,0,0,0.1), 10, 0, 0, 2);");
+        webViewContainer.setAlignment(javafx.geometry.Pos.CENTER);  //center our editing area
+        webViewContainer.getChildren().add(webView);
+        webViewContainer.setFocusTraversable(true); //makes container able to receive events
+
+        // Configuring the ScrollPane() which contains the stackPane()
+        scrollPane = new ScrollPane();
+        scrollPane.setContent(webViewContainer);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);     //Never want horizontal scrolling
+    }
+
+    // Defining where to place toolbar and editing area
+    private void initializeLayout(){
+        mainLayout = new BorderPane();
+        mainLayout.setTop(toolBar.getView());
+        mainLayout.setCenter(scrollPane);
+    }
+
+    private void forceRepaint() {
+        // Use a tiny delay to ensure the 'format' operation is
+        // fully committed to the Quill history stack before we bounce it.
+        PauseTransition delay = new PauseTransition(Duration.millis(50));
+        delay.setOnFinished(e -> {
+            quill.executeScript(
+                    "if (quill.history.stack.undo.length > 0) {" +
+                            "    quill.history.undo();" +
+                            "    quill.history.redo();" +
+                            "}"
+            );
+
+            // Final JavaFX nudge: sub-pixel resize forces the window to redraw
+            double w = webView.getWidth();
+            webView.setPrefWidth(w + 0.01);
+            Platform.runLater(() -> webView.setPrefWidth(w));
+        });
+        delay.play();
+    }
+
+    public Editor(String name) {
+        this.name = name;
+        dpi = readDPI();
+        toolBar = new Toolbar(giveClipboardOperations(), giveFunctionsWithParams());
+        initializeWebView();
+        StyleContainer();
+        initializeLayout();
+    }
+
+    //Shortcut functions
+    private void undo(){
+        Platform.runLater(() -> {
+            // Cutoff ensures that whatever was being typed right now
+            // is "sealed" before the undo command moves the timeline back.
+            quill.executeScript("quill.history.cutoff(); quill.history.undo();");
+        });
+    }
+
+    private void redo(){
+        Platform.runLater(() -> {
+            quill.executeScript("quill.history.cutoff(); quill.history.redo();");
+        });
+    }
+
+    private void toolBarFontSize(String size, String source){
+        Platform.runLater(() -> {
+            quill.executeScript("quill.focus();" +
+                    "quill.history.cutoff();" +
+                    "quill.format('size'," + size + ",'" + source + "');" +
+                    "quill.history.cutoff();"
+            );
+            forceRepaint();
+        });
+
+    }
+
+    private void fontSizeShortcut(String operand, String source){
+        Platform.runLater(() -> {
+            quill.executeScript("quill.focus();" +
+                            "quill.focus();" +
+                            "quill.history.cutoff();" +
+                            "var currentSize = quill.getFormat().size;" +
+                            "if (!currentSize || !currentSize.endsWith('px')) {" +
+                            "    currentSize = '13px';" +
+                            "}" +
+                            "var number = parseInt(currentSize);" +
+                            "var newSize = (number " + operand + " 2) + 'px';" +
+                            "quill.format('size', newSize, '" + source + "');" +
+                            "quill.history.cutoff();"
+            );
+            forceRepaint();
+        });
+
+    }
+
+    private void applyScript(String scriptType, String source){
+        Platform.runLater(() -> {
+           quill.executeScript("quill.focus();" +
+                   "quill.history.cutoff();" +
+                   "quill.history.cutoff();" +
+                   "var currentScript = quill.getFormat().script;" +    // Get the current script format ('sub', 'super', or undefined)
+                   // If it matches the button we clicked, turn it off (false). Otherwise, turn it on.
+                   "var newValue = (currentScript === '" + scriptType + "') ? false : '" + scriptType + "';" +
+                   "quill.format('script', newValue, '" + source + "');" +
+                   "quill.history.cutoff();"
+           );
+           forceRepaint();
+        });
+    }
+
+    private void format(String type, String source){
+        Platform.runLater(() -> {
+            quill.executeScript(
+                    "quill.focus();" +
+                            "quill.history.cutoff();" +
+                            "quill.format('" + type + "', !quill.getFormat()." + type + ", '" + source + "');" +
+                            "quill.history.cutoff(); "
+            );
+            forceRepaint();
+        });
+
+    }
+
+    private void initializeShortcuts(){
+        webView.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (event.isControlDown() || event.isMetaDown()) {  //ctrl (windows), cmd (mac)
+                switch (event.getCode()) {
+                    case Z:
+                        event.consume();    // Prevents event from being passed to quill
+                        if (event.isShiftDown()) {
+                            redo();
+                        } else {
+                            undo();
+                        }
+                        break;
+                    case Y:
+                        event.consume();
+                        redo();
+                        break;
+                    case B:
+                        event.consume();
+                        format("bold", "user");
+                        break;
+                    case I:
+                        event.consume();
+                        format("italic", "user");
+                        break;
+                    case U:
+                        event.consume();
+                        format("underline", "user");
+                        break;
+                    case K:
+                        event.consume();
+                        format("link", "user");
+                        break;
+                    case EQUALS:
+                        event.consume();
+                        if(event.isShiftDown()){    //ctrl-shift-plus
+                            fontSizeShortcut("+", "user");
+                        }
+                        break;
+                    case MINUS:
+                        event.consume();
+                        if(event.isShiftDown()){    //ctrl-shift-minus
+                            fontSizeShortcut("-", "user");
+                        }
+                        break;
+                }
+            }
+        });
+    }
+
+    // Getters & Setters
+    public BorderPane getView() {
+        return mainLayout;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDPI(){
+        return dpi;
+    }
+
+    public ClipboardHandler getClipboardHandler() {
+        return clipboardHandler;
+    }
+
+    // These two functions provide the Toolbar object limited access to the webEngine (quill)
+    private HashMap<String, BiConsumer<String, String>> giveFunctionsWithParams(){
+        HashMap<String, BiConsumer<String, String>> temp = new HashMap<>();
+        temp.put("format", this::format);
+        temp.put("setFontSize", this::toolBarFontSize);
+        temp.put("applyScript", this::applyScript);
+        return temp;
+    }
+
+    private HashMap<String, Runnable> giveClipboardOperations(){
+        HashMap<String, Runnable> temp = new HashMap<>();
+        temp.put("undo", this::undo);
+        temp.put("redo", this::redo);
+        return temp;
+    }
+}
